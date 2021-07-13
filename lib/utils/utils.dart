e6a2cf825399d1a132d89f59084fe589e4f76db8
Avisha-7 <66829119+Avisha-7@users.noreply.github.com>
2021-07-14 02:26:23 +0530

    added comments

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,9 +1,11 @@
 class Utils {
+  
+  // get Username from the email used to sign in
   static String getUsername(String email) {
     return "live:${email.split('@')[0]}";
-
   }
 
+  // get the initials from first and last name of the registered user
   static String getInitials(String name) {
     List<String> nameSplit = name.split(" ");
     String firstNameInitial = nameSplit[0][0];
