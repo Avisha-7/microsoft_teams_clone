3e28f56f7414be84f435edde47e40958285b3753
Avisha-7 <66829119+Avisha-7@users.noreply.github.com>
2021-07-14 02:17:30 +0530

    removed dead code

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -4,19 +4,15 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:microsoft_clone/main.dart';
-import 'package:microsoft_clone/screens/call_screen.dart';
 import 'package:microsoft_clone/screens/chat_screen.dart';
 import 'package:microsoft_clone/screens/profile_screen.dart';
 import 'package:microsoft_clone/utils/color_scheme.dart';
-
-import '../variables.dart';
 import 'contact_screen.dart';
 import 'meet_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   final String currentUserId;
   HomeScreen({Key? key, required this.currentUserId}) : super(key: key);
-
 
   @override
   _HomeScreenState createState() => _HomeScreenState();
@@ -33,32 +29,6 @@ class _HomeScreenState extends State<HomeScreen> {
         MaterialPageRoute(builder: (context) => MyApp()), (
         Route<dynamic> route) => false);
   }
-  // late PageController pageController;
-  // int _page=0;
-  //
-  // @override
-  // void initState(){
-  //   super.initState();
-  //   pageController=PageController();
-  // }
-
-  // void onPageChanged(int page){
-  //   setState(() {
-  //     // set the current page value to the page value that we receive from the callback
-  //     _page = page;
-  //   });
-  // }
-
-  // void navigationTapped(int page){
-  //   pageController.jumpToPage(page);
-  // }
-
-  List<String> _options=[
-    "CHAT",
-    "MEET",
-    "CONTACTS"
-    "PROFILE"
-  ];
 
   List pageOptions = [
     ChatScreen(),
@@ -73,50 +43,7 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor:colorVariables.homePageTheme,
-
-      // BottomNavigationBar(
-      //   backgroundColor: Colors.white,
-      //   selectedItemColor: Colors.blue,
-      //   selectedLabelStyle:appStyle(17,Colors.black),
-      //   unselectedItemColor: Colors.black,
-      //   unselectedLabelStyle: appStyle(17,Colors.black),
-      //   currentIndex:page,
-      //   onTap:(index){
-      //     setState(() {
-      //       page=index;
-      //     });
-      //   },
-      //   items:[
-      //     BottomNavigationBarItem(
-      //         label:'Meet',
-      //         icon: Icon(Icons.video_call, size:32)
-      //     ),
-      //     BottomNavigationBarItem(
-      //         label:'Profile',
-      //         icon: Icon(Icons.person, size:32)
-      //     ),
-      //   ],
-      //
-      // ),
       body: pageOptions[_currentIndex],
-      // Container(
-      //   color:colorVariables.backgroundTheme,
-      //   child:Center(
-      //     child:Text(_options[_currentIndex],
-      //     )
-      //   ),
-      // ),
-        // PageView(
-        //   children:<Widget> [
-        //     Center(child:Text("Chat List Screen")),
-        //     Center(child:Text("Call Logs Screen")),
-        //     Center(child:Text("Meet Screen")),
-        //     Center(child:Text("Contacts List Screen")),
-        //   ],
-        //   controller: pageController,
-        //   onPageChanged: onPageChanged,
-        // ),
-      // pageOptions[_page],
       bottomNavigationBar: CurvedNavigationBar(
         buttonBackgroundColor: Colors.white,
         backgroundColor: Color(0xFF886CE4),
@@ -134,8 +61,6 @@ class _HomeScreenState extends State<HomeScreen> {
             _currentIndex=index;
           });
         },
-        // onTap: navigationTapped,
-        // currentIndex: _page,
       ),
     );
   }
